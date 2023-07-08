41bbeda150342c9b9be296c7a27bd4fd6a026a66
HarishFullStack <harishgupta.dev@gmail.com>
2023-07-08 22:04:44 +0530

    added add review modal

--- src/pages/RestaurantDetails.js
+++ src/pages/RestaurantDetails.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router";
+import { useNavigate, useParams } from "react-router-dom";
 import { restaurantsData} from '../db/data';
 import { Modal } from "react-bootstrap";
 
@@ -10,6 +10,8 @@ export function RetaurantDetails(){
 
     const [restaurant, setRestaurant] = useState();
     const [showAddRreviewModal, setShowAddReviewModal] = useState(false);
+    const [selectedRating, setRating] = useState();
+    const [selectedComment, setComment] = useState("");
 
     const getRestaurants = () => {
         setRestaurant(restaurantsData.find((x) => x.id === Number(restaurantId)));
@@ -21,7 +23,10 @@ export function RetaurantDetails(){
     }, [])
 
     const handleAddReview = () => {
-        
+        const updatedRestaurant = {...restaurant, ratings: [...restaurant.ratings, {rating: selectedRating, comment: selectedComment, renName: "Riya", pp: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5tbKdv1HDbAjPc526SK0yDZuoOmaaOyGNoj_e1q3ngruK2bTqzub3&s=0"}]};
+        setRestaurant(updatedRestaurant);
+        setRating();
+        setComment("");
     }
 
     return(
@@ -49,25 +54,25 @@ export function RetaurantDetails(){
             <Modal show={showAddRreviewModal} onHide={() => setShowAddReviewModal(false)}  size="lg">
                 <Modal.Header>
                     <Modal.Title>
-                        Edit Post
+                        Add Your Review
                     </Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                     <div>
-                    Rating: <select className="form-control mb-5 col-md-6">
+                    Rating: <select className="form-control mb-5 col-md-6" onChange={(event) => setRating(event.target.value)}>
                         <option>Select Rating</option>
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
                     </select>
-                    Comments:    <textarea className="post-area w-100 form-control" />
+                    Comments:    <textarea className="post-area w-100 form-control"  onChange={(event) => setComment(event.target.value)}/>
                 </div>
                 </Modal.Body>
                 <Modal.Footer>
                     <button type="button" className="btn btn-secondary" onClick={() => setShowAddReviewModal(false)}>Close</button>
-                    <button type="button" className="btn btn-primary" onClick={handleAddReview}>Update</button>
+                    <button type="button" className="btn btn-primary" disabled={selectedRating === undefined || selectedComment === "" } onClick={() => [handleAddReview(), setShowAddReviewModal(false)]}>Update</button>
                 </Modal.Footer>
         </Modal>
         </div>
